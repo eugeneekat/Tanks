17106d50743be95d300867f9041c102aa23428d1
EugeneEkat <ekaterinenko88@gmail.com>
2016-10-13 11:44:21 +0300

    Tanks

--- Tanks/Program.cs
+++ Tanks/Program.cs
@@ -10,6 +10,7 @@ namespace Tanks
     {
         static void Main(string[] args)
         {
+            //Изображение игрока и компьютера
             string[] leftTankSpr = {"        \n",
                                     "------  \n",
                                     "|   ----\n",
@@ -23,20 +24,27 @@ namespace Tanks
                                     "----   |\n",
                                     "  ------\n",
                                     "        \n"};
+            //Устанавливает размеры поля
             int width = 50;
             int height = 20;
             GameField.SetField(width, height);
 
+            //Создаем класс отрисовки
             Display disp = new Display();
+            //Класс управление игрока
             PlayerController playerContoller = new PlayerController();
+            //Класс танка игрока
             Tank playerTank = new Tank(0, 0, leftTankSpr);
+            //Подписка танка на события контроллера
             playerContoller.move    += playerTank.OnMove;
             playerContoller.shoot   += playerTank.OnShoot;
             playerContoller.exit    += playerTank.OnExit;
+            //Подписка отрисовщика на события танка
             playerTank.move         += disp.OnMoveUpdate;
             playerTank.shoot        += disp.OnShootUpdate;
             playerTank.exit         += disp.Exit;
 
+            //Тоже самое что у игрока
             AIController aiController = new AIController();
             Tank aiTank = new Tank(GameField.MaxWidth, 0, rightTankSpr);
             aiController.move   += aiTank.OnMove;
@@ -46,13 +54,14 @@ namespace Tanks
             aiTank.shoot        += disp.OnShootUpdate;
             aiTank.exit         += disp.Exit;
 
+            //Запуск асинхронных действий компьютера и игрока
             aiController.AsyncAction();
             playerContoller.AsyncAction();
-            
-            while(!GameField.IsEndGame)
-            {
-                
-            }
+
+            //Бесконечный цикл пока игра не закончена
+            while (!GameField.IsEndGame) { }
+
+            //Считать нажатие так как поток игрока в режиме ожидания нажатия клавишы
             Console.ReadKey();
             
         }
